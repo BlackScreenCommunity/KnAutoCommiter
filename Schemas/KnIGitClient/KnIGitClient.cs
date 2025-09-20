c01bb8e2d47df2956c8928b3062c98e3bfe93286
NKuzmin <nkuzmin@korusconsulting.ru>
2025-09-20 21:53:40 +0300

    Изменение интерефейса для клиента работы с git

--- Schemas/KnIGitClient/KnIGitClient.cs
+++ Schemas/KnIGitClient/KnIGitClient.cs
@@ -1,19 +1,17 @@
  namespace BPMSoft.Configuration
 {
-    using System;
-    using LibGit2Sharp;
-    using LibGit2Sharp.Handlers;
+    using System.Threading;
+    using System.Threading.Tasks;
 
-	public interface IGitClient
-	{
-		Task CloneAsync(string url, string workdir, CancellationToken ct = default);
-		Task InitAsync(string workdir, CancellationToken ct = default);
-		Task<string> GetCurrentBranchAsync(string workdir, CancellationToken ct = default);
-		Task<string> StatusAsync(string workdir, CancellationToken ct = default);
-		Task FetchAsync(string workdir, string remote = "origin", CancellationToken ct = default);
-		Task PullAsync(string workdir, string remote = "origin", string branch = "main", CancellationToken ct = default);
-		Task AddAllAndCommitAsync(string workdir, string message, string? authorName = null, string? authorEmail = null, CancellationToken ct = default);
-		Task CreateTagAsync(string workdir, string tagName, string? message = null, CancellationToken ct = default);
-	}
-	 
- }
+    public interface IGitClient
+    {
+        Task CloneAsync(string url, string workdir, CancellationToken ct = default(CancellationToken));
+        Task InitAsync(string workdir, CancellationToken ct = default(CancellationToken));
+        Task<string> GetCurrentBranchAsync(string workdir, CancellationToken ct = default(CancellationToken));
+        Task<string> StatusPorcelainAsync(string workdir, CancellationToken ct = default(CancellationToken));
+        Task FetchAsync(string workdir, string remote, CancellationToken ct = default(CancellationToken));
+        Task PullFastForwardAsync(string workdir, string remote, string branch, CancellationToken ct = default(CancellationToken));
+        Task AddAllAndCommitAsync(string workdir, string message, string authorName, string authorEmail, CancellationToken ct = default(CancellationToken));
+        Task CreateTagAsync(string workdir, string tagName, string message, CancellationToken ct = default(CancellationToken));
+    }
+}
