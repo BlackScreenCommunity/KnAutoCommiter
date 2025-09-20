d216fb1dab703f7bb52932179ca100db500f8a0b
NKuzmin <nkuzmin@korusconsulting.ru>
2025-09-20 21:59:53 +0300

    Добавлено консольное приложение для запуска сформированной библиотеки без запуска BPMSoft

---
+++ KnAutoCommiterRunner/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace KnAutoCommiterRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new BPMSoft.Configuration.KnCommiterService();
+            Console.WriteLine(service.GetPing());
+        }
+    }
+}
