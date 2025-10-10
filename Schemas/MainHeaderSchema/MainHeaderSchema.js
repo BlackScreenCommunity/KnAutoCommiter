21b4fcd574b459cfff1ba43a242fd53ea6776f03
NKuzmin <nkuzmin@korusconsulting.ru>
2025-10-10 19:19:11 +0300

    Изменена иконка на кнопке

--- Schemas/MainHeaderSchema/MainHeaderSchema.js
+++ Schemas/MainHeaderSchema/MainHeaderSchema.js
@@ -1,48 +1,55 @@
 define("MainHeaderSchema", ["KnGitGuiMixin"], function () {
 	return {
 		mixins: {
-			KnGitGuiMixin: "BPMSoft.KnGitGuiMixin"
+			KnGitGuiMixin: "BPMSoft.KnGitGuiMixin",
 		},
-		
+
 		methods: {
-			onGitGuiButtonClicked: function() {
+			onGitGuiButtonClicked: function () {
 				console.log("Show window and call service");
 				this.mixins.KnGitGuiMixin.showModalBox();
-			}
+			},
 		},
 		diff: [
 			{
-				"operation": "insert",
-				"name": "GitGuiContainer",
-				"parentName": "InnerRightButtonsContainer",
-				"propertyName": "items",
-				"values": {
-					"id": "header-git-gui-container",
-					"itemType": BPMSoft.ViewItemType.CONTAINER,
-					"wrapClass": ["context-git-gui-class"],
-					"items": [],
-					// "visible": {"bindTo": "IsSystemDesignerVisible"}
-				}
+				operation: "remove",
+				name: "ContextHelpContainer",
 			},
 			{
-				"operation": "insert",
-				"name": "GitGuiButton",
-				"parentName": "GitGuiContainer",
-				"propertyName": "items",
-				"values": {
-					"id": "view-button-system-designer",
-					"itemType": BPMSoft.ViewItemType.BUTTON,
-					"selectors": {
-						"wrapEl": "git-client-open-button"
+				operation: "insert",
+				name: "GitGuiContainer",
+				parentName: "InnerRightButtonsContainer",
+				propertyName: "items",
+				values: {
+					id: "header-git-gui-container",
+					itemType: BPMSoft.ViewItemType.CONTAINER,
+					wrapClass: ["context-git-gui-class"],
+					items: [],
+					// "visible": {"bindTo": "IsSystemDesignerVisible"}
+				},
+			},
+			{
+				operation: "insert",
+				name: "GitGuiButton",
+				parentName: "GitGuiContainer",
+				propertyName: "items",
+				values: {
+					id: "view-button-system-designer",
+					itemType: BPMSoft.ViewItemType.BUTTON,
+					selectors: {
+						wrapEl: "git-client-open-button",
 					},
-					"classes": {
-						"wrapperClass": ["system-designer-button"],
-						"pressedClass": ["pressed-button-view"],
-						"imageClass": ["system-designer-image", "view-images-class"]
+					classes: {
+						wrapperClass: ["system-designer-button"],
+						pressedClass: ["pressed-button-view"],
+						imageClass: [
+							"system-designer-image",
+							"view-images-class",
+						],
 					},
-					"tips": [],
-					"click": {
-						"bindTo": "onGitGuiButtonClicked"
+					tips: [],
+					click: {
+						bindTo: "onGitGuiButtonClicked",
 					},
 					// "canExecute": {
 					// 	"bindTo": "canBeDestroyed"
@@ -50,13 +57,15 @@ define("MainHeaderSchema", ["KnGitGuiMixin"], function () {
 					// "visible": {
 					// 	"bindTo": "IsSystemDesignerButtonVisible"
 					// },
-					"imageConfig": {"bindTo": "Resources.Images.SystemDesignerIcon"},
-					"style": this.BPMSoft.controls.ButtonEnums.style.TRANSPARENT,
-					"iconAlign": this.BPMSoft.controls.ButtonEnums.iconAlign.LEFT,
-					"markerValue": "git-client-open-button",
-					"tag": "git-client-open-button"
-				}
+					imageConfig: {
+						bindTo: "Resources.Images.GitClientIcon",
+					},
+					style: this.BPMSoft.controls.ButtonEnums.style.TRANSPARENT,
+					iconAlign: this.BPMSoft.controls.ButtonEnums.iconAlign.LEFT,
+					markerValue: "git-client-open-button",
+					tag: "git-client-open-button",
+				},
 			},
-		]
+		],
 	};
 });
